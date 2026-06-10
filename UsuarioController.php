56f96ac9f02f409779e654ebbdb6d751226d0df7
Gabriela <gabiavila1124@gmail.com>
2026-06-09 22:00:21 -0400

    Create UsuarioController.php

---
+++ UsuarioController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function index()
+    {
+		return view('usuarios.index');
+	}
+	public function criar()
+	{
+		return view('usuarios.criar');
+	}
+}

349830f4b2e3158ce78459a92f3f9d8eea083945
Gabriela <gabiavila1124@gmail.com>
2026-06-09 22:48:09 -0400

    Update iindex.blade.php

--- iindex.blade.php
+++ iindex.blade.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Usuários - PoliData</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body class="usuarios-page d-flex flex-column min-vh-100" >
 	<header>

27f1efdd25bf6d0aa36ef4da22265ca7c40448a2
Gabriela <gabiavila1124@gmail.com>
2026-06-11 23:05:46 -0400

    Update UsuarioController.php

--- UsuarioController.php
+++ UsuarioController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Usuario;
 
 class UsuarioController extends Controller
 {
     public function index()
     {
-		return view('usuarios.index');
+		$usuarios = Usuario::all();
+		
+		return view('usuarios.index', compact('usuarios'));
 	}
 	public function criar()
 	{

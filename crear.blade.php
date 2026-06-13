db81f8fa4db6258702a4b537df97c2e97fb169d8
Gabriela <gabiavila1124@gmail.com>
2026-06-13 11:38:42 -0400

    Create crear.blade.php

---
+++ crear.blade.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\PerfilController;
+
+Route::get('/', function () {
+	return view('index');
+});
+
+Route::get('/usuarios', 
+[UsuarioController::class, 'index']);
+
+Route::get('/usuarios/criar', 
+[UsuarioController::class, 'criar']);
+
+Route::post('/usuarios', [UsuarioController::class, 'salvar']);
+
+Route::get('/perfis', [PerfilController::class, 'index']);
+
+Route::get('/perfis/editar', [PerfilController::class, 'editar']);
+
